19dc4c3d255cb111b04f224d174f0918ddcad683
Pinky Sodhi <pinky.sodhi@niit.com>
2022-11-08 12:40:42 +0530

    created getter and setter for the Manager type

--- src/main/java/com/niit/jdp/Manager.java
+++ src/main/java/com/niit/jdp/Manager.java
@@ -8,4 +8,20 @@ package com.niit.jdp;
 public class Manager extends Employee {
     int teamSize;
     int teamRatings;
+
+    public int getTeamSize() {
+        return teamSize;
+    }
+
+    public void setTeamSize(int teamSize) {
+        this.teamSize = teamSize;
+    }
+
+    public int getTeamRatings() {
+        return teamRatings;
+    }
+
+    public void setTeamRatings(int teamRatings) {
+        this.teamRatings = teamRatings;
+    }
 }
