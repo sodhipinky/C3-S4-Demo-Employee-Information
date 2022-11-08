63450343b0df381b60bd4c0c9d8b80460b2771a0
Pinky Sodhi <pinky.sodhi@niit.com>
2022-11-08 15:05:17 +0530

    added constructors to the RegionalHead type

--- src/main/java/com/niit/jdp/RegionalHead.java
+++ src/main/java/com/niit/jdp/RegionalHead.java
@@ -5,9 +5,27 @@
  */
 package com.niit.jdp;
 
+import java.time.LocalDate;
+
 public class RegionalHead extends Employee {
     String regionName;
     int regionPerformance;
     int reviewRatings;
 
+    public RegionalHead() {
+    }
+
+    public RegionalHead(String name,
+                        int age,
+                        LocalDate dateOfBirth,
+                        String address,
+                        double salary,
+                        String regionName,
+                        int regionPerformance,
+                        int reviewRatings) {
+        super(name, age, dateOfBirth, address, salary);
+        this.regionName = regionName;
+        this.regionPerformance = regionPerformance;
+        this.reviewRatings = reviewRatings;
+    }
 }
