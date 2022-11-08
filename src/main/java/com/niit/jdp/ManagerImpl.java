9243ecc3e2004862ea06d479cd5fb45182235f76
Pinky Sodhi <pinky.sodhi@niit.com>
2022-11-08 12:39:41 +0530

    created an implementation class for Manager and created an Object of the manager type

---
+++ src/main/java/com/niit/jdp/ManagerImpl.java
@@ -0,0 +1,12 @@
+/*
+ * Author : Pinky Sodhi
+ * Date : 08-11-2022
+ * Created Using : IntelliJ IDEA Community Edition
+ */
+package com.niit.jdp;
+
+public class ManagerImpl {
+    public static void main(String[] args) {
+        Manager manager = new Manager();
+    }
+}
