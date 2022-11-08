e40c555f265d7d3e40300d8f989961795f6d7bd8
Pinky Sodhi <pinky.sodhi@niit.com>
2022-11-08 12:36:16 +0530

    created Employee.java and added fields, constructors and getter setter methods

---
+++ src/main/java/com/niit/jdp/Employee.java
@@ -0,0 +1,68 @@
+/*
+ * Author : Pinky Sodhi
+ * Date : 08-11-2022
+ * Created Using : IntelliJ IDEA Community Edition
+ */
+package com.niit.jdp;
+
+import java.time.LocalDate;
+
+public class Employee {
+    //properties
+    String name;
+    int age;
+    LocalDate dateOfBirth;
+    String address;
+    double salary;
+
+    public Employee() {
+    }
+
+    public Employee(String name, int age, LocalDate dateOfBirth, String address, double salary) {
+        this.name = name;
+        this.age = age;
+        this.dateOfBirth = dateOfBirth;
+        this.address = address;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
